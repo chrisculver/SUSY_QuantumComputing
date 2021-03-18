f2119186a27e6d4ef906fe2fbdcff2fd554e1e84
chrisculver <chris.cb.culver@gmail.com>
2021-03-18 18:42:27 +0000

    Hopefully removed ipynb checkpoints

--- .ipynb_checkpoints/BinaryEncodings-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def standard_encode(i,N):
-    format_style = '0' + str(N) + 'b'
-    return format(i, format_style)
-
-
-def gray_code(i,N):
-    gray=i^(i>>1)
-    return "{0:0{1}b}".format(gray,N)
