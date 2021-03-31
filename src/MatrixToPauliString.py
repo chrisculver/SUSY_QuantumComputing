274fa029a12a3e44501cef3b36b4b669961104a9
chrisculver <chris.cb.culver@gmail.com>
2021-03-31 22:31:07 +0100

    Matrix to pauli string is now just functions

--- src/MatrixToPauliString.py
+++ src/MatrixToPauliString.py
@@ -17,59 +17,59 @@ def basis_to_pauli_string(i, j, q):
         return 0.5*symbols('I^'+q) - 0.5*symbols('Z^'+q)
 
 
-class MatrixToPauliString:
-    def __init__(self, matrix):
-        self.matrix=matrix #row-col ordering, m[i][j] i-th row, j-th col
-        self.N = len(self.matrix)
-        self.pauli_strings=0
-        self.pauli_terms = defaultdict(complex)
+def matrix_to_pauli_strings(matrix, encoding):
+    #row-col ordering, m[i][j] i-th row, j-th col
+    N = len(matrix)
+    pauli_strings=0
+    for i in range(0, N):
+        for j in range(0, N):
+            pauli_strings += convert_element(matrix[i][j], i, j, N, encoding)
     
-    def convert(self, encoding):
-        for i in range(0, self.N):
-            for j in range(0, self.N):
-                self.pauli_strings += self.convert_element(i, j, encoding)
-    
-    def convert_element(self, i, j, encoding):
-        nBinDigits = len(format(self.N-1,'b'))
-        i_bin = encoding(i, nBinDigits)
-        j_bin = encoding(j, nBinDigits)
-        qubit_product = 1
-        
-        for q in range(0,len(i_bin)):
-            qubit_product *= basis_to_pauli_string(i_bin[q], j_bin[q], str(len(i_bin)-1-q))
+    return pauli_strings
+
+
+def convert_element(elem, i, j, N, encoding):
+    nBinDigits = len(format(N-1,'b'))
+    i_bin = encoding(i, nBinDigits)
+    j_bin = encoding(j, nBinDigits)
+    qubit_product = 1
+       
+    for q in range(0,len(i_bin)):
+        qubit_product *= basis_to_pauli_string(i_bin[q], j_bin[q], str(len(i_bin)-1-q))
             
-        return self.matrix[i][j]*qubit_product
+    return elem*qubit_product
     
     
-    def pauli_strings_as_list(self):
-        self.pauli_strings = expand(self.pauli_strings)
-        #goes through all terms in pauli_strings (aka all a_i in sum_i a_i)
-        for arg in self.pauli_strings.args:
-            #each term is a product of a number * paulis, convert each elem to list
-            arg_list=sympy_expr_to_list(arg)
+def pauli_strings_to_list(pauli_strings):
+    pauli_strings = expand(pauli_strings)
+    ps_list = defaultdict(complex)
+    #goes through all terms in pauli_strings (aka all a_i in sum_i a_i)
+    for arg in pauli_strings.args:
+        #each term is a product of a number * paulis, convert each elem to list
+        arg_list=sympy_expr_to_list(arg)
 
 
-            paulis = [ '' for i in range(0,len(arg_list)-1) ]
-            start = 1
-            coef = complex(arg_list[0])
+        paulis = [ '' for i in range(0,len(arg_list)-1) ]
+        start = 1
+        coef = complex(arg_list[0])
 
-            if(len(str(arg_list[1]))==1):
-                coef*=1j
-                start=2
-                paulis.remove('')
+        if(len(str(arg_list[1]))==1):
+            coef*=1j
+            start=2
+            paulis.remove('')
 
-            for ai in range(start, len(arg_list)):
-                symbol = str(arg_list[ai])
-                parts = symbol.split('^')
-                paulis[ int(parts[1]) ] = parts[0]
+        for ai in range(start, len(arg_list)):
+            symbol = str(arg_list[ai])
+            parts = symbol.split('^')
+            paulis[ int(parts[1]) ] = parts[0]
 
-            key = ''
-            for i in paulis:
-                key += i
+        key = ''
+        for i in paulis:
+            key += i
 
-            self.pauli_terms[key] += coef
+        ps_list[key] += coef
         
-        return self.pauli_terms
+    return ps_list
     
 
     
